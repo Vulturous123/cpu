c87312906a11656c396b3e66187c6d39a1643348
Vulturous123 <79800723+Vulturous123@users.noreply.github.com>
2021-03-13 15:58:57 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qw70uyze3s9vxjacma4k9ak80wxfa4d00ku5q40.test1
 
 
 done
